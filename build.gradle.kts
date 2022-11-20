08fc919939c2108000890cba37e21d9ad224280e
Paul Schuberth <github@pschuberth.de>
2022-11-20 21:58:10 +0100

    Fix publishing

--- build.gradle.kts
+++ build.gradle.kts
@@ -38,8 +38,8 @@ publishing {
             name = "GitHub"
             url = uri("https://maven.pkg.github.com/paulschuberth/mdgen")
             credentials {
-                username = System.getenv("GITHUB_ACTOR")
-                password = System.getenv("GITHUB_TOKEN")
+                username = project.findProperty("gpr.user") as String? ?: System.getenv("USERNAME")
+                password = project.findProperty("gpr.key") as String? ?: System.getenv("TOKEN")
             }
         }
     }
@@ -49,7 +49,6 @@ publishing {
         }
     }
 }
-
 tasks.javadoc {
     if (JavaVersion.current().isJava9Compatible) {
         (options as StandardJavadocDocletOptions).addBooleanOption("html5", true)
